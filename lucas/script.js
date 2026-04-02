b38818ea95b5db3c79c48d9b599396fd6e20b4fb
Lopeszzx <lucas.vinhaes@escola.pr.gov.br>
2026-04-02 16:29:04 -0300

    Add files via upload

---
+++ lucas/script.js
@@ -0,0 +1,14 @@
+function alterarNome() {
+
+
+    let trocarNome = document.getElementById("inputNome").value;
+
+
+    if (trocarNome === '') {
+        alert('Digite um nome!')
+    } else {
+        document.getElementById('nomeUsuario').innerHTML = trocarNome;
+    }
+
+
+}
